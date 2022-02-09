beb24914a581d9cd56ce5a05748621a348b12c26
Luis Chamberlain <mcgrof@kernel.org>
2022-02-09 11:00:06 -0800

    snprintf: specify safe fallthrough on switches
    gcc -Wall will complain when we fallthrough on a switch
    but don't mean it. Fortunately we can follow the Linux kernel
    strategy to use -Wimplicit-fallthrough=2 and allow comments
    to supress this to clarify this was intended.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- snprintf.c
+++ snprintf.c
@@ -317,6 +317,7 @@ static size_t dopr(char *buffer, size_t maxlen, const char *format, va_list args
 				break;
 			case 'X':
 				flags |= DP_F_UP;
+				/* fallthrough */
 			case 'x':
 				flags |= DP_F_UNSIGNED;
 				if (cflags == DP_C_SHORT)
@@ -339,6 +340,7 @@ static size_t dopr(char *buffer, size_t maxlen, const char *format, va_list args
 				break;
 			case 'E':
 				flags |= DP_F_UP;
+				/* fallthrough */
 			case 'e':
 				if (cflags == DP_C_LDOUBLE)
 					fvalue = va_arg (args, LDOUBLE);
@@ -348,6 +350,7 @@ static size_t dopr(char *buffer, size_t maxlen, const char *format, va_list args
 				break;
 			case 'G':
 				flags |= DP_F_UP;
+				/* fallthrough */
 			case 'g':
 				if (cflags == DP_C_LDOUBLE)
 					fvalue = va_arg (args, LDOUBLE);
